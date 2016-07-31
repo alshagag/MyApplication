daaf09e164ac7770927e648b5ff0dd41d568efa2
abu3li <hossainalyousif@gmail.com>
2016-08-01 02:23:58 +0300

    حذف الملفات الزائدة

--- app/src/main/java/com/example/mmhh2/myapplication/MainActivity.java
+++ app/src/main/java/com/example/mmhh2/myapplication/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.mmhh2.myapplication;
 
-import android.content.Intent;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,23 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btnopen = (Button)findViewById(R.id.op_act);
-        Button btnopen2 = (Button)findViewById(R.id.btnop2);
-        btnopen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this,Main2Activity.class);
-                startActivity(i);
-            }
-        });
 
-        btnopen2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this,Main3Activity.class);
-                startActivity(i);
-            }
-        });
 
     }
 
