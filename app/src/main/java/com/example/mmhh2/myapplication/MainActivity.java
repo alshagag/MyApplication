bebe386814882957dd9aafd2147c34669b82707e
abu3li <hossainalyousif@gmail.com>
2016-08-01 22:17:11 +0300

    تعديل الملفا

--- app/src/main/java/com/example/mmhh2/myapplication/MainActivity.java
+++ app/src/main/java/com/example/mmhh2/myapplication/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.mmhh2.myapplication;
 
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,10 +17,12 @@ import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
+    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 0;
     protected Button btnCharge, btnCheck;
     protected EditText numCard;
     protected RelativeLayout mainActivity;
-
+    private long ID = 0;
+    public String sCharge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +55,18 @@ public class MainActivity extends AppCompatActivity {
                     if (numstring.matches("[0-9]+")) {
                         if (numstring.length() >= 14) {
                             long numberCard = Long.parseLong(numstring);
+                            sCharge = "*1400*" + numberCard + "*" + ID + "#";
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                                        if (!shouldShowRequestPermissionRationale(Manifest.permission.CALL_PHONE)) {
+                                            requestPermissions(new String[]{Manifest.permission.CALL_PHONE},MY_PERMISSIONS_REQUEST_CALL_PHONE);
+                                        }
+
+                                    return;
+                                }
+                            }
+                            call(sCharge);
+
                         } else {
                             Toast.makeText(getBaseContext(), "رقم البطاقة قصير جدا", Toast.LENGTH_SHORT).show();
                         }
@@ -61,11 +81,57 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        btnCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sCharge = "*1411#";
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                        if (!shouldShowRequestPermissionRationale(Manifest.permission.CALL_PHONE)) {
+                            requestPermissions(new String[]{Manifest.permission.CALL_PHONE},MY_PERMISSIONS_REQUEST_CALL_PHONE);
+                        }
 
+                        return;
+                    }
+                }
+                call(sCharge);
+            }
+        });
 
     }
 
+    protected void call(String num) {
+        Intent iChatge = new Intent(Intent.ACTION_CALL, Uri.fromParts("tel",num,null));
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
 
+            return;
+        }
+        startActivity(iChatge);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CALL_PHONE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    call(sCharge);
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                break;
+            }
+
+            default:
+                super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+        }
+    }
 }
 
 
