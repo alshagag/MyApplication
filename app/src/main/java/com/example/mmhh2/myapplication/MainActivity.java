0f2285e18844a9cbfcb2012161732201c2a6e716
abu3li <hossainalyousif@gmail.com>
2016-08-01 16:44:21 +0300

    تعديل الملفات واضافة

--- app/src/main/java/com/example/mmhh2/myapplication/MainActivity.java
+++ app/src/main/java/com/example/mmhh2/myapplication/MainActivity.java
@@ -3,15 +3,57 @@ package com.example.mmhh2.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 //test1
 
 public class MainActivity extends AppCompatActivity {
-
+    public Button btnCharge,btnCheck;
+    public EditText numCard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnCharge = (Button)findViewById(R.id.btnCharge);
+        btnCheck = (Button)findViewById(R.id.btnCheck);
+        numCard = (EditText)findViewById(R.id.numCard);
 
+
+        btnCharge.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction() == MotionEvent.ACTION_UP){
+
+                    EditText num = (EditText)view.findViewById(R.id.numCard);
+                    num.setText("");
+                    return true;
+                }
+
+                return false;
+            }
+        });
+        btnCharge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String numstring = numCard.getText().toString();
+                if(!numstring.isEmpty()){
+                    if (numstring.length() >= 14){
+                        long numberCard = Long.parseLong(numstring);
+                    }
+                    else {
+                        Toast.makeText(getBaseContext(),"رقم البطاقة قصير جدا",Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+                else {
+                    Toast.makeText(getBaseContext(),"رقم البطاقة فارغ",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
 
     }
 
